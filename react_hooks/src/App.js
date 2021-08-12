f92654a8e4a16592aaaeac564afed143b09f81b2
Manjushree Berike Rajanna <manju_br@Manjushrees-MBP.attlocal.net>
2021-08-12 16:58:24 -0700

    After reactBasics

---
+++ react_hooks/src/App.js
@@ -0,0 +1,95 @@
+import React, {Component, useState, useEffect} from "react"
+
+const App = () => {
+  const [news, setNews] = useState([])
+  const [searchQuery, setSearchQuery] = useState('react')
+  const [url, setUrl] = useState('http://hn.algolia.com/api/v1/search?query=react')
+  const [loading, setLoading] = useState(false);
+
+
+  //fetch news
+
+  const fetchNews = () => {
+    setLoading(true);
+    fetch(url)
+    .then(result => result.json())
+    .then(data => (setNews(data.hits), setLoading(false)))
+    .catch(error => console.log(error));
+
+  };
+
+  useEffect(() => {
+
+    fetchNews()
+  }, [url])
+
+  const handleChange = (e) => {
+    setSearchQuery(e.target.value)
+
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    setUrl(`http://hn.algolia.com/api/v1/search?query=${searchQuery}`);
+
+  }
+const showLoading = () => ( loading ? <h2>Loading...</h2> : "")
+
+const showForm = () => (<form onSubmit={handleSubmit}> 
+  <input type="text" value={searchQuery} onChange={handleChange}/>
+  <button >Search</button>
+</form>)
+
+const showNews = () => news.map((n,i) => <p key={i}>{n.title}</p>)
+
+
+return(
+<div>
+  <h2> News</h2>
+  {showLoading()}
+  {showForm()}
+  {showNews()}
+ 
+
+</div>
+
+
+);
+
+
+
+
+};
+
+
+
+
+
+// const App = ()=>{
+// const [count, setCount] = useState(0)
+
+// useEffect(() => {
+
+//   document.title = `Clicked ${count} times`
+// })
+
+
+// const increment = () =>{
+//   setCount(count + 1)
+// }
+// return (
+//   <div>
+//       <h1>counter App</h1>
+//          <button onClick={increment}>
+//             Clicked {count} times
+
+//          </button>
+     
+  
+//   </div>
+// );
+
+// }
+
+
+
+export default App;
